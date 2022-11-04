281e6eeadd69207b0d26cca344a6d7fde98e6daa
a179346 <r122164152@gmail.com>
2022-11-05 02:22:28 +0800

    init tailwind

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
+      </body>
+    </Html>
+  );
+}
